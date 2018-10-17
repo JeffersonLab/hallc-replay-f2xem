e0d75070d0ae646392b2e8588e35d64c960957a6
Abishek Karki <ak1508@msstate.edu>
2018-10-17 10:17:59 -0400

    restoring file as master

--- CALIBRATION/dc_calib/scripts/main_calib.C
+++ CALIBRATION/dc_calib/scripts/main_calib.C
@@ -5,19 +5,13 @@
 #include <ctime>
 using namespace std;
 
-int main_calib(Int_t run_NUM, Int_t even_NUM)
+int main_calib()
 {
 
   //prevent root from displaying graphs while executing
   gROOT->SetBatch(1);
 
-   TString filename = "/w/hallc-scifs17exp/xem2/abishek/hallc-replay-f2xem/ROOTfiles/shms_replay_production_all_";
 
-  //TString filename = "/volatile/hallc/xem2/abels/ROOTfiles/hms_replay_production_all_";
-  filename += run_NUM;
-  filename += "_";
-  filename += even_NUM;
-  filename += ".root";
   //measure execution time
   clock_t cl;
   cl = clock();
@@ -26,9 +20,9 @@ int main_calib(Int_t run_NUM, Int_t even_NUM)
                                                                                                         // |
                                                                                                         // v
   //  DC_calib obj("HMS", "../../../ROOTfiles/hms_replay_production_all_1856_hodtrefcut1000_-1.root", 1856,-1, "pid_elec", "card");
- 
-    DC_calib obj("SHMS", filename, run_NUM, even_NUM, "pid_elec", "card"); 
-  
+  //DC_calib obj("SHMS", "../../../ROOTfiles/shms_replay_production_all_2774_-1.root", 2774, -1, "pid_elec"); 
+  //  DC_calib obj("SHMS", "~/abishek/hallc_replay/ROOTfiles/shms_replay_production_all_1791_-1.root", 1791, 10000000, "pid_elec", "card");
+    DC_calib obj("SHMS", "../../../ROOTfiles/shms_replay_production_all_1791dcuncal_-1.root", 1791, 10000000, "pid_elec", "card");                                                                                                        
   // DC_calib obj("HMS", "../../../ROOTfiles/hms_coin_replay_production_1866_1000000.root", 1866, 1000, "pid_kFALSE");
   
   obj.setup_Directory();
